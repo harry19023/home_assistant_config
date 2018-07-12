3958b013ea4b4156c398318e96bea02e48ed2f06
Harry Metzger <harry19023@gmail.com>
2018-07-12 16:50:20 -0700

    changed the computer_off method

--- appDaemon/apps/computer_control.py
+++ appDaemon/apps/computer_control.py
@@ -1,31 +1,6 @@
 import appdaemon.plugins.hass.hassapi as hass
 import secrets
-from paramiko import client
-
-class ssh:
-  client = None
-
-  def __init__(self, address, username, password):
-    print("Connecting to server.")
-    self.client = client.SSHClient()
-    self.client.set_missing_host_key_policy(client.AutoAddPolicy())
-    self.client.connect(address, username=username, password=password, look_for_keys=False)
-
-  def sendCommand(self, command):
-    if(self.client):
-      stdin, stdout, stderr = self.client.exec_command(command)
-      while not stdout.channel.exit_status_ready():
-        # Print data when available
-        if stdout.channel.recv_ready():
-          alldata = stdout.channel.recv(1024)
-          prevdata = b"1"
-          while prevdata:
-            prevdata = stdout.channel.recv(1024)
-            alldata += prevdata
-
-          print(str(alldata, "utf8"))
-    else:
-      print("Connection not opened.")
+import requests
 
 
 class ComputerControl(hass.Hass):
@@ -41,6 +16,5 @@ class ComputerControl(hass.Hass):
     self.log('turned computer on', level='INFO')
 
   def computer_off(self):
-    connection =ssh(secrets.LivingRoom_IP ,"LivingRoom", secrets.LivingRoom_password)
-    connection.sendCommand("shutdown /h")
+    r = requests.get("http://" + secrets.LivingRoom_IP  + secrets.LivingRoom_Hibernate)
     self.log('turned computer off', level='INFO')
