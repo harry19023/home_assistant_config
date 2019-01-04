0348bb6d1417f3775ceabd4c616198f8a51bcb68
Harry Metzger <harrym@cybcube.com>
2019-01-03 17:02:37 -0800

    added living room motion light override in front end (for guests)

--- appDaemon/apps/motion_light.py
+++ appDaemon/apps/motion_light.py
@@ -19,14 +19,19 @@ class MotionLight(hass.Hass):
     fan = self.get_state('input_boolean.in_bed')
     status = self.get_state(self.light)
     home = self.get_state('input_boolean.home')
+    living_room_motion = self.get_state('input_boolean.living_room_motion')
     if home == 'on':
       if status == 'off':
+        if self.light == 'light.living_room' and living_room_motion == 'off':
+          self.log('Living Room not turned on due to override', level='INFO')
+          return
         if fan == 'on':
           if self.light == 'light.bedroom':
             self.log('Bedroom motion detected, but fan on so lights stay off', level='INFO')
             return
-          self.turn_on(self.light, rgb_color=self.globals.nightlightRGB, brightness=self.globals.nightlightBrightness, transition=1)
-          self.log('Turned on ' + self.light + ' dim because fas was on and motion was detected', level='INFO')
+          else:
+            self.turn_on(self.light, rgb_color=self.globals.nightlightRGB, brightness=self.globals.nightlightBrightness, transition=1)
+            self.log('Turned on ' + self.light + ' dim because fas was on and motion was detected', level='INFO')
         else:
           self.turn_on(self.light)
         # self.log('Turned on ' + self.light + ' because motion was detected', level='INFO')
